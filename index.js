8f06a52ad2958c5f1ec65de0ae8546544c7bb256
Fadhly <fadhlyrobbani1986@gmail.com>
2021-12-09 11:34:37 +0800

    ganti nama

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     header.classList.toggle('scrolling-active', window.scrollY > 0);
 });
+document
+.querySelector('.menu-btn')
+.addEventListener('click',() => 
+document.querySelector('.nav-list').classList.toggle('show')
+);
